27d5fda11586e5368fe82b9e30e753dbbc24dc5e
aleynaisikdaglilar <aleynaisikdaglilar@gmail.com>
2023-09-26 19:16:25 +0300

    Package added for LocalSPM

--- Sources/ButtonPackage/ButtonPackage.swift
+++ Sources/ButtonPackage/ButtonPackage.swift
@@ -1,6 +1,24 @@
-public struct ButtonPackage {
-    public private(set) var text = "Hello, World!"
+//public struct ButtonPackage {
+//    public private(set) var text = "Hello, World!"
+//
+//    public init() {
+//    }
+//}
 
-    public init() {
+
+import UIKit
+
+public class AlertPackage: UIAlertController {
+    
+    public func alert(fromController controller: UIViewController) {
+        
+        // create the alert
+        let alert = UIAlertController(title: "My Title", message: "This is my message.", preferredStyle: UIAlertController.Style.alert)
+        
+        // add an action (button)
+        alert.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
+        
+        // show the alert
+        controller.present(alert, animated: true, completion: nil)
     }
 }
--- Tests/ButtonPackageTests/ButtonPackageTests.swift
+++ Tests/ButtonPackageTests/ButtonPackageTests.swift
@@ -6,6 +6,6 @@ final class ButtonPackageTests: XCTestCase {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
-        XCTAssertEqual(ButtonPackage().text, "Hello, World!")
+//        XCTAssertEqual(ButtonPackage().text, "Hello, World!")
     }
 }
